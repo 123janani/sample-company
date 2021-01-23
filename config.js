4bcaceb77c41eee41dba134404b9e52149215709
Janani Bodaragama <32632854+123janani@users.noreply.github.com>
2021-01-23 18:45:27 +0530

    Update config.js
    code refactored

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ mysqlConnection.connect((err)=> {
     if(!err)
         console.log('Database Connection Established Successfully');
     else
-        console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
+        console.log('Connection Failed!'+ JSON.stringify(err));
 });
 
 module.exports = mysqlConnection;
