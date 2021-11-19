613cdd004fa4c546236d387d2bc7a55f755e626b
123janani <ict1415008>
2021-11-19 22:08:56 +0530

    test

--- model/user.model.js
+++ model/user.model.js
@@ -101,7 +101,7 @@ user.deleteUser = (id) => {
     return new Promise( (resolve, reject) => {
     db.query("DELETE FROM user WHERE userId = ?", [id], (err, res)=> {
         if(err) {
-            console.log("errorrrr: ", err);
+            console.log("errorrr: ", err);
             reject(err);
         }
         else{
